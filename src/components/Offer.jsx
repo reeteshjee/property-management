8685a02e2d895a1b8c15abd064f058af55ce0747
Nomin <droaemon7711@gmail.com>
2025-03-17 21:17:57 -0500

    status added

--- src/components/Offer.jsx
+++ src/components/Offer.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
 
-const Offer = ({ property, offers, onAcceptOffer, onRejectOffer }) => {
+export default function Offer({
+  property,
+  offers,
+  onAcceptOffer,
+  onRejectOffer,
+}) {
   return (
     <div className="bg-white shadow-lg rounded-lg overflow-hidden">
-      <img src="https://photos.zillowstatic.com/fp/e1ad17b30ff057a60f24e1b45b718401-p_e.webp" alt="Modern Villa"/>
+      <img
+        src="https://photos.zillowstatic.com/fp/e1ad17b30ff057a60f24e1b45b718401-p_e.webp"
+        alt="Modern Villa"
+      />
       <div className="p-6">
-        <h3 className="text-xl font-semibold text-gray-800">{property.title}</h3>
+        <h3 className="text-xl font-semibold text-gray-800">
+          {property.title}
+        </h3>
         <p className="text-gray-500 text-sm mb-4">{property.description}</p>
         <div className="text-lg font-bold text-gray-900">{`$${property.price}`}</div>
 
@@ -15,8 +25,12 @@ const Offer = ({ property, offers, onAcceptOffer, onRejectOffer }) => {
           <div className="mt-4">
             {offers.map((offer, index) => (
               <div key={index} className="bg-gray-100 p-4 rounded-lg mb-4">
-                <p><strong>Offer Amount:</strong> ${offer.offerAmount}</p>
-                <p><strong>Message:</strong> {offer.offerMessage}</p>
+                <p>
+                  <strong>Offer Amount:</strong> ${offer.offerAmount}
+                </p>
+                <p>
+                  <strong>Message:</strong> {offer.offerMessage}
+                </p>
 
                 {/* Accept or Reject Offer */}
                 <div className="mt-4 flex gap-4">
@@ -42,6 +56,4 @@ const Offer = ({ property, offers, onAcceptOffer, onRejectOffer }) => {
       </div>
     </div>
   );
-};
-
-export default Offer;
+}
